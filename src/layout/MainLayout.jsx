8ccd272cb4625e2173af6b7d342382885dec6eca
alim1381 <ali.moaydi2020@gmail.com>
2024-02-19 23:17:13 +0330

    debug

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/header/header";
+import Header from "../components/header/Header";
 import LeftMenu from "../components/Menu/LeftMenu";
 import Footer from "../components/footer/Footer";
 import { Outlet } from "react-router-dom";
